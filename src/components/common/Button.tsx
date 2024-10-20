9a0b7b10da588cd18bbf6155dae79678c2c3fc0a
onblana <51261847+onblana@users.noreply.github.com>
2024-10-20 11:35:01 +0900

    chore: 폴더 구조 초기 세팅

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,8 @@
+export default function Button() {
+  return (
+    <>
+      <p>This is Empty Component</p>
+      <p>Make your own component</p>
+    </>
+  );
+}
---
+++ src/types/type.d.ts
@@ -0,0 +1,9 @@
+type SomeType1 = {
+  id: number;
+  address: string;
+};
+
+interface SomeType2 {
+  id: number;
+  name: string;
+}
