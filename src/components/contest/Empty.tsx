a5fb653e05cda16e946f18d239934cb807a043c5
onblana <51261847+onblana@users.noreply.github.com>
2024-10-22 17:33:05 +0900

    feat: Add group-based filtering functionality

--- src/components/contest/Empty.tsx
+++
@@ -1,8 +0,0 @@
-export default function Empty() {
-  return (
-    <>
-      <p>This is Empty Component</p>
-      <p>Make your own component</p>
-    </>
-  );
-}
---
+++ src/components/contest/FilterReset.tsx
@@ -0,0 +1,3 @@
+export default function FilterReset() {
+  return <button className="">필터 초기화</button>;
+}
