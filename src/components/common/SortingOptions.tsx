28554ed8f08270c4bdb45458bab4d79c65ff5ec9
onblana <51261847+onblana@users.noreply.github.com>
2024-10-24 18:26:32 +0900

    feat: Style reset button UI

--- src/components/common/SortingOptions.tsx
+++ src/components/common/SortingOptions.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
@@ -11,23 +12,34 @@ export default function SortingOptions() {
   };
 
   return (
-    <div className="flex items-center space-x-2 ml-[16px] mt-[10px]">
-      {options.map((option, index) => (
-        <div key={option} className="flex items-center text-[20px]">
-          <span
-            className={twMerge(
-              "cursor-pointer",
-              selectedOption === option ? "text-red-500" : "text-gray-200"
+    <div className="flex items-center justify-between space-x-2 ml-[16px] mt-[10px]">
+      <div className="inline-flex">
+        {options.map((option, index) => (
+          <div key={option} className="flex items-center text-[20px]">
+            <span
+              className={twMerge(
+                "cursor-pointer",
+                selectedOption === option ? "text-red-500" : "text-gray-200"
+              )}
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </span>
+            {index < options.length - 1 && (
+              <span className="text-gray-100 ml-6 mr-6">|</span>
             )}
-            onClick={() => handleOptionClick(option)}
-          >
-            {option}
-          </span>
-          {index < options.length - 1 && (
-            <span className="text-gray-100 ml-6 mr-6">|</span>
-          )}
-        </div>
-      ))}
+          </div>
+        ))}
+      </div>
+      <button className="flex gap-2.5 pr-8 items-center">
+        초기화
+        <Image
+          src="/images/reset_icon.svg"
+          alt="reset_button"
+          width={17}
+          height={17}
+        />
+      </button>
     </div>
   );
 }
