1ffd4a7be52359498d44b8426649d69e5fe1aa6a
onblana <51261847+onblana@users.noreply.github.com>
2024-10-24 13:55:42 +0900

    refactor: Apply twMerge library for class name management

--- src/components/contest/FilterOption.tsx
+++ src/components/contest/FilterOption.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import images from "@/assets/images/images";
+import { twMerge } from "tailwind-merge";
 
 export default function FilterOption({
   option,
@@ -9,19 +11,18 @@ export default function FilterOption({
   toggled: boolean;
   onToggle: () => void;
 }) {
+  const baseClasses =
+    "relative border text-[16px] px-3 py-1 rounded-full transition-colors";
+  const toggledClasses = toggled
+    ? "bg-red-500 text-white border-red-500"
+    : "bg-white text-gray-700 border-gray-300 hover:border-gray-400";
+
   return (
-    <button
-      className={`relative border text-[16px] px-3 py-1 rounded-full transition-colors ${
-        toggled
-          ? "bg-red-200 text-white border-red-200"
-          : "bg-white text-gray-700 border-gray-300 hover:border-gray-400"
-      }`}
-      onClick={onToggle}
-    >
+    <button className={twMerge(baseClasses, toggledClasses)} onClick={onToggle}>
       {toggled && (
         <div className="absolute -top-[7px] left-1/2 transform -translate-x-1/2">
           <Image
-            src="/images/tomato-stem.svg"
+            src={images.tomatoStem}
             width={20}
             height={20}
             alt="Tomato stem"
