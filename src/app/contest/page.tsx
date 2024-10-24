81f1353377fc9338a564c6cc65c551e9b75d1061
onblana <51261847+onblana@users.noreply.github.com>
2024-10-24 16:06:10 +0900

    chore: Move Filter components to "common" folder

--- src/app/contest/page.tsx
+++ src/app/contest/page.tsx
@@ -1,10 +1,12 @@
-import Filter from "@/components/contest/Filter";
+import Filter from "@/components/common/Filter";
+import SortingOptions from "@/components/common/SortingOptions";
 
 export default function ContestPage() {
   return (
-    <>
+    <div className="container">
       <h1 className="text-[32px] font-recipe">공모전</h1>
       <Filter />
-    </>
+      <SortingOptions />
+    </div>
   );
 }
---
+++ src/components/common/SortingOptions.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function SortingOptions() {
+  const [selectedOption, setSelectedOption] = useState("최신순");
+  const options = ["관련도순", "최신순", "조회순", "마감순"];
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      {options.map((option, index) => (
+        <div key={option} className="flex items-center text-[20px]">
+          <span
+            className={twMerge(
+              "cursor-pointer",
+              selectedOption === option ? "text-red-500" : "text-gray-200"
+            )}
+            onClick={() => handleOptionClick(option)}
+          >
+            {option}
+          </span>
+          {index < options.length - 1 && (
+            <span className="text-gray-100 ml-6 mr-6">|</span>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
